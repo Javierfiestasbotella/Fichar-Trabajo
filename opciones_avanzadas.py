10b5a10b4ff49a428662432ab0e062d28e7f3d16
Javier Fiestas <javierfiestasbotella@gmail.com>
2023-03-31 17:20:47 +0200

    31/03/2023

--- opciones_avanzadas.py
+++ opciones_avanzadas.py
@@ -118,4 +118,4 @@ ar=open('pruebas python\\Fichar_tiempo\\octubre\\2022-10-3_.txt','r')
 print(ar.read())
 ar.close()'''
 
-total_horas_archivo_Antiguo("2022-11-26.txt")
+total_horas_archivo_Antiguo("C:\\Users\\Usuario\\Desktop\\python\\Fichar-Trabajo\\2023-03-25.txt")
